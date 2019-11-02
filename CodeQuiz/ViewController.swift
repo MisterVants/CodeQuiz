14cb3de5570246b9361ca6e9a6c44dd2964a41e8
MrVants <andrevants@gmail.com>
2019-11-02 13:23:19 -0300

    Xcode project commit

---
+++ CodeQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeQuiz
+//
+//  Created by André Vants Soares de Almeida on 02/11/19.
+//  Copyright © 2019 André Vants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
